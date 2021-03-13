1717dd6fc309edc04c4d1481cc938fe17652515a
Anton <79669605+Anton-Viktorov@users.noreply.github.com>
2021-03-13 15:10:37 +0300

    Add files via upload

---
+++ Module_10/fifth_element.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Умножить константу BRUCE_WILLIS на пятый элемент строки, введенный пользователем
+
+try:
+    BRUCE_WILLIS = 42
+
+    input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
+except ValueError as e:
+    print(f"Ошибка преобразования в целое число. {e.args}")
+except IndexError as e:
+    print(f"Входной список слишком короткий. Нет элемента с индексом 4")
+except Exception as e:
+    print(f"Непредвиденная ошибка. Информация: {e}")
